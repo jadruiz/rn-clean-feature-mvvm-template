756b3d10efa94608dba05ac2e7e322d34145f4c3
José R <ed.alanpoeta@gmail.com>
2025-02-17 23:00:46 -0600

    refactor(utils): Refactoriza las utilidades de fechas y crea debounce en performance.

---
+++ src/core/utils/performance/debounce.ts
@@ -0,0 +1,17 @@
+// src/core/utils/performance/debounce.ts
+
+/**
+ * Retorna una versión debounced de la función dada, que se ejecutará
+ * solo después de que transcurra un retraso especificado sin nuevas invocaciones.
+ *
+ * @param func La función que se quiere debounced.
+ * @param delay El retraso en milisegundos (por defecto 300ms).
+ * @returns Una función debounced.
+ */
+export const debounce = (func: (...args: any[]) => void, delay = 300) => {
+  let timer: NodeJS.Timeout;
+  return (...args: any[]) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => func(...args), delay);
+  };
+};
